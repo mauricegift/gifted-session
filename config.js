ac9cc7c7420c0f54348e66453cdc7bc8c85337d0
Maurice Gift <maurice@giftedtech.co.ke>
2026-04-05 22:58:48 +0300

    🚀 Sync from VPS — remove credentials

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT || 50900,
     SESSION_PREFIX: process.env.SESSION_PREFIX || "Gifted~",
-    GC_JID: process.env.GC_JID || "LZE4CoZNhLB28z5jtqwNLA",
-    DATABASE_URL: process.env.DATABASE_URL || "", // Your Db URL here(optional). Can either be mongodb or postreSQL
+    GC_JID: process.env.GC_JID,
+    DATABASE_URL: process.env.DATABASE_URL,
     BOT_REPO: process.env.BOT_REPO || "https://github.com/mauricegift/atassa",
-    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029Vb6lNd511ulWbxu1cT3A",
+    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VbCpYtZLtOj5LDuj7Q1p",
     MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢɪғᴛᴇᴅ ᴛᴇᴄʜ*",
 };
