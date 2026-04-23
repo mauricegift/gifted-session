50d456d4d771935aa4425f6ac3dbde22534554a8
Lucvoice <simkondabrand@gmail.com>
2026-04-23 09:38:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ require('dotenv').config({
 
 module.exports = {
     PORT: process.env.PORT || 50900,
-    SESSION_PREFIX: process.env.SESSION_PREFIX || "Gifted~",
+    SESSION_PREFIX: process.env.SESSION_PREFIX || "Luka~",
     GC_JID: process.env.GC_JID || "GuS93JhyfyE56LOV3ZJTFZ",
     DATABASE_URL: process.env.DATABASE_URL || "",
     BOT_REPO: process.env.BOT_REPO || "https://github.com/mauricegift/atassa",
     WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VbCpYtZLtOj5LDuj7Q1p",
-    MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢɪᶠᴛᴇᴅ ᴛᴇᴄʜ*",
+    MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ LUKA ᴛᴇᴄʜ*",
 };
